75fe55456cf482d0bf3d7cd09ad1bfc304a869a7
Martin <mrtn.mtth@gmx.de>
2014-03-22 23:31:21 +0100

    move all styling into style tags

--- tmpl/default.php
+++ tmpl/default.php
@@ -6,21 +6,46 @@ $moduleclass_sfx = $params->get( 'moduleclass_sfx' );
 echo '<style>
 table.modReports {
 	border-collapse: collapse;
-	width:auto;
+	width: 100%;
+}
+tr.modReports, td.modReports {
+	text-align: left;
+	padding: 2px 5px;
+	color: #'.$colortext.';
+}
+tr.modReports_dotted {
+	border-bottom: #000000 1px dotted;
+}
+td.modReports span {
+	font-weight: bold;
+}
+td.modReports a {
+	text-decoration: none;';
+	
+if ($display[templatecolor] != '1') echo 'color: #'.$coloralert.';';
+
+echo '}
+img.modReports {
+	margin: 2px 2px 2px 2px;
+	padding: 2px 2px 2px 2px;
+	height: auto;
+	width: '.$bild_breite.';
+	float: '.$bild_float.';
+	border: '.$bild_border.';
 }
 </style>';
 
 if ($count>count($frontReports))
 	$count=count($frontReports);
 
-echo '<table style="width:100%;" class="modReports" class="'.$moduleclass_sfx;
+echo '<table class="modReports" class="'.$moduleclass_sfx;
 if ($display[umbruch] == '0')
 	echo '" nowrap="nowrap"';
 echo '>';
 
 for($i=0; $i < $count; $i++)
 {
-	echo '<tr style="text-align:left;padding: 2px 5px;color:#'.$colortext.';">';
+	echo '<tr class="modReports">';
 
 	$frontReports[$i]->summary =
 		(strlen($frontReports[$i]->summary) > $display[maxchar]) ?
@@ -29,9 +54,8 @@ for($i=0; $i < $count; $i++)
 		$frontReports[$i]->summary;
  
 	$curTime = strtotime($frontReports[$i]->date1);
-	echo '<td style="text-align:left;padding: 2px 5px;color:#'.$colortext.';">';
-	echo '<span style="font-weight:bold;">'.date('d.m.Y', $curTime)
-		.'</span>&nbsp;&nbsp;';
+	echo '<td class="modReports">';
+	echo '<span>'.date('d.m.Y', $curTime).'</span>&nbsp;&nbsp;';
 
 	if ($display[date1] == '1')
 		echo 'um '.date('H:i', $curTime).' Uhr&nbsp;&nbsp;';
@@ -41,10 +65,7 @@ for($i=0; $i < $count; $i++)
 
 	if ($display[data1] == '1')
 	{
-		echo '<a style="';
-		if ($display[templatecolor] != '1')
-			echo 'color:#'.$coloralert.';';
-		echo 'text-decoration:none;" href="'.$link[$i].'">';
+		echo '<a href="'.$link[$i].'">';
 		echo '<b>'.$frontReports[$i]->data1.'&nbsp;&nbsp;</b></a>';
 		if ($display[umbruch] == '1')
 			echo '<br/>';
@@ -62,7 +83,7 @@ for($i=0; $i < $count; $i++)
 		echo $frontReports[$i]->summary;
 
 	if ($readontext)
-		echo '<a style="color:#'.$coloralert.';text-decoration:none;" href="'.$link[$i].'">&nbsp;'.$readontext.'</a>';
+		echo '<a href="'.$link[$i].'">&nbsp;'.$readontext.'</a>';
 
 	echo '</td>';
 
@@ -72,10 +93,10 @@ for($i=0; $i < $count; $i++)
 	if (($bild=='1') and ($foto[$i]))
 	{
 		echo '<td><a href="'.$link[$i].'">';
-		echo '<img src="'.$baseUploadDir.'/'.$foto[$i].'" style="margin: 2px 2px 2px 2px; padding:2px 2px 2px 2px;height:auto;width:'.$bild_breite.'; float:'.$bild_float.'; border:'.$bild_border.';" /></a></td>';
+		echo '<img class="modReports" src="'.$baseUploadDir.'/'.$foto[$i].'" /></a></td>';
 	}
 
-	echo '</tr><tr style="border-bottom: #000000 1px dotted;"></tr>';
+	echo '</tr><tr class="modReports_dotted"></tr>';
 }
 echo '</table>';
 
