380d30a53533dc9bc57c52134440e9e04f140a90
Martin <mrtn.mtth@gmx.de>
2014-03-23 01:10:17 +0100

    use divs instead of tables for template

--- tmpl/default.php
+++ tmpl/default.php
@@ -8,20 +8,22 @@ table.modReports {
 	border-collapse: collapse;
 	width: 100%;
 }
-tr.modReports, td.modReports {
+div.modReports {
+	float: left;
 	text-align: left;
-	padding: 2px 5px;';
+	padding: 2px 5px 5px;';
 	
 if ($display[templatecolor] != '1') echo 'color: #'.$colortext.';';
 
 echo '}
-tr.modReports_dotted {
-	border-bottom: #000000 1px dotted;
+hr.modReports {
+	color: transparent;
+	clear: both;
 }
-td.modReports span {
+modReports span {
 	font-weight: bold;
 }
-td.modReports a {
+modReports a {
 	text-decoration: none;';
 	
 if ($display[templatecolor] != '1') echo 'color: #'.$coloralert.';';
@@ -38,15 +40,8 @@ img.modReports {
 if ($count>count($frontReports))
 	$count=count($frontReports);
 
-echo '<table class="modReports" class="'.$moduleclass_sfx;
-if ($display[umbruch] == '0')
-	echo '" nowrap="nowrap"';
-echo '>';
-
 for($i=0; $i < $count; $i++)
 {
-	echo '<tr class="modReports">';
-
 	$frontReports[$i]->summary =
 		(strlen($frontReports[$i]->summary) > $display[maxchar]) ?
 		substr($frontReports[$i]->summary,0,strrpos(substr
@@ -54,7 +49,7 @@ for($i=0; $i < $count; $i++)
 		$frontReports[$i]->summary;
  
 	$curTime = strtotime($frontReports[$i]->date1);
-	echo '<td class="modReports">';
+	echo '<div class="modReports">';
 	echo '<span>'.date('d.m.Y', $curTime).'</span>&nbsp;&nbsp;';
 
 	if ($display[date1] == '1')
@@ -85,19 +80,17 @@ for($i=0; $i < $count; $i++)
 	if ($readontext)
 		echo '<a href="'.$link[$i].'">&nbsp;'.$readontext.'</a>';
 
-	echo '</td>';
+	echo '</div>';
 
 	if ($bild_float=='none')
-		echo '</tr><tr>';
+		echo '<hr class="modReports">';
 
 	if (($bild=='1') and ($foto[$i]))
 	{
-		echo '<td><a href="'.$link[$i].'">';
-		echo '<img class="modReports" src="'.$baseUploadDir.'/'.$foto[$i].'" /></a></td>';
+		echo '<div><a href="'.$link[$i].'">';
+		echo '<img class="modReports" src="'.$baseUploadDir.'/'.$foto[$i].'" /></a></div>';
 	}
-
-	echo '</tr><tr class="modReports_dotted"></tr>';
+	echo '<hr class="modReports">';
 }
-echo '</table>';
 
 ?>
