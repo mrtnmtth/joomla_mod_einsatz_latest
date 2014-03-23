b7866503ae82a2fb02a4a847d2a0b5fe21647c7a
Martin <mrtn.mtth@gmx.de>
2014-03-23 22:37:50 +0100

    Fixed previous commit and some other css issues. Shortened some if-clauses.

--- tmpl/default.php
+++ tmpl/default.php
@@ -3,14 +3,10 @@ defined('_JEXEC') or die('Restricted Access');
 ?>
 
 <style>
-table.modReports2Ticker {
-	border-collapse: collapse;
-	width: 100%;
-}
 div.modReports2Ticker {
 	text-align: left;
 	padding: 2px 5px 5px;
-	<?php if ($display['templatecolor'] != '1') echo 'color: #'.$colortext.';'; ?>
+	<?php if ($display['templatecolor'] != '1') echo 'color: #'.$colortext.';'."\n"; ?>
 }
 hr.modReports2Ticker {
 	color: transparent;
@@ -18,32 +14,31 @@ hr.modReports2Ticker {
 }
 hr.separator1 {
 	border: 0;
-	border-bottom: 1px dashed <?php echo '.$separatorcolor.'; ?>
+	border-bottom: 1px dashed <?php echo $separatorcolor; ?>;
 }
 hr.separator2 {
 	border: 0;
-	border-bottom: 1px solid <?php echo '.$separatorcolor.'; ?>
+	border-bottom: 1px solid <?php echo $separatorcolor; ?>;
 }
 hr.separator3 {
 	border: 0;
 	height: 1px;
-	background-image: -webkit-linear-gradient(left, rgba(0,0,0,0), <?php echo '.$separatorcolor.'; ?>, rgba(0,0,0,0));
-	background-image:    -moz-linear-gradient(left, rgba(0,0,0,0), <?php echo '.$separatorcolor.'; ?>, rgba(0,0,0,0));
-	background-image:     -ms-linear-gradient(left, rgba(0,0,0,0), <?php echo '.$separatorcolor.'; ?>, rgba(0,0,0,0));
-	background-image:      -o-linear-gradient(left, rgba(0,0,0,0), <?php echo '.$separatorcolor.'; ?>, rgba(0,0,0,0));
+	background-image: -webkit-linear-gradient(left, rgba(0,0,0,0), <?php echo $separatorcolor; ?>, rgba(0,0,0,0));
+	background-image:    -moz-linear-gradient(left, rgba(0,0,0,0), <?php echo $separatorcolor; ?>, rgba(0,0,0,0));
+	background-image:     -ms-linear-gradient(left, rgba(0,0,0,0), <?php echo $separatorcolor; ?>, rgba(0,0,0,0));
+	background-image:      -o-linear-gradient(left, rgba(0,0,0,0), <?php echo $separatorcolor; ?>, rgba(0,0,0,0));
 }
-modReports2Ticker span {
+div.modReports2Ticker span {
 	font-weight: bold;
 }
-modReports2Ticker a {
-	text-decoration: none;';
-	<?php if ($display['templatecolor'] != '1') echo 'color: #'.$coloralert.';'; ?>
+div.modReports2Ticker a {
+	<?php if ($display['templatecolor'] != '1') echo 'color: #'.$coloralert.' !important;'."\n"; ?>
 }
-img.modReports2Ticker {
+div.modReports2Ticker img {
 	margin: 2px 10px 2px 10px;
 	height: auto;
-	width: <?php echo '.$bild_breite.'; ?>
-	float: <?php echo '.$bild_float.'; ?>
+	width: <?php echo $bild_breite; ?>;
+	float: <?php echo $bild_float; ?>;
 }
 </style>
 
@@ -59,40 +54,37 @@ for($i=0; $i < $count; $i++)
 	if (($bild=='1') and ($foto[$i]))
 	{
 		echo '<a href="'.$link[$i].'">';
-		echo '<img class="modReports2Ticker" src="'.$baseUploadDir.'/'.$foto[$i].'" />';
+		echo '<img src="'.$baseUploadDir.'/'.$foto[$i].'" />';
 		echo '</a>';
 	}
 	echo '<p><span>'.date('d.m.Y', $curTime).'</span>&nbsp;&nbsp;';
 
-	if ($display['date1'] == '1')
+	if ($display['date1'])
 		echo 'um '.date('H:i', $curTime).' Uhr&nbsp;&nbsp;';
 
-	if ($display['umbruch'] == '1')
+	if ($display['umbruch'])
 		echo '<br/>';
 
-	if ($display['data1'] == '1')
+	if ($display['data1'])
 	{
 		echo '<a href="'.$link[$i].'">';
 		echo '<b>'.$frontReports[$i]->data1.'</b></a>&nbsp;&nbsp;';
-		if ($display['umbruch'] == '1')
-			echo '<br/>';
+		if ($display['umbruch']) echo '<br/>';
 	}
 
-	if ($display['address'] == '1')
+	if ($display['address'])
 	{
 		echo 'in '.$frontReports[$i]->address.'&nbsp;&nbsp;';
-		if ($display['umbruch'] == '1')
-			echo '<br/>';
+		if ($display['umbruch']) echo '<br/>';
 	}
    
-	if ($display['summary'] == '1')
+	if ($display['summary'])
 	{
 		echo $frontReports[$i]->summary.'&nbsp;&nbsp;';
-		if ($display['umbruch'] == '1')
-			echo '<br/>';
+		if ($display['umbruch']) echo '<br/>';
 	}
 
-	if ($display['desc'] == '1')
+	if ($display['desc'])
 		echo $frontReports[$i]->desc.'&nbsp;';
 
 	if ($readontext)
