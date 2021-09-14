6e9682e4832df7ecd75b1902ddbefeb9f135bca3
mrtnmtth <mrtn.mtth@gmx.de>
2021-09-14 21:41:05 +0200

    fix: remove unused configuration options

--- mod_einsatz_latest.php
+++ mod_einsatz_latest.php
@@ -9,25 +9,10 @@ defined('_JEXEC') or die('Illegal Access');
 JLoader::register('ModEinsatzLatestHelper', __DIR__ . '/helper.php');
 
 $moduleclass_sfx = $params->get('moduleclass_sfx', '');
-$modulewidth = $params->get('modulewidth', '100%');
 
 $count = $params->get('count', '5');
 $menuNone = $params->get('menu_none', 'No Reports Found');
 $maxchar = $params->get('maxchar', '250');
-$readontext = $params->get('readontext', '');
-$separator = $params->get('separator', '1');
-$separatorcolor = $params->get('separatorcolor', '#333');
-
-$bild = $params->get('bild', '1');
-$bild_breite = $params->get('bild_breite', '1');
-$bild_float = $params->get('bild_float', '1');
-
-$display['einsatzart'] = $params->get('einsatzart', '1');
-$display['address'] = $params->get('address', '0');
-$display['date1'] = $params->get('date1', '1');
-$display['summary'] = $params->get('summary', '1');
-$display['desc'] = $params->get('desc', '0');
-$display['maxchar'] = $params->get('maxchar', '1');
 
 $menulink = JComponentHelper::getParams('com_einsatzkomponente')->get('homelink');
 $frontReports = modEinsatzLatestHelper::getReports($count);
